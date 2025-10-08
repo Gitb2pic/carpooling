88716cedc64614cc75db5261fa863c9ec824718d
b2pic <alexmpamidev@gmail.com>
2025-10-08 20:38:10 +0100

    Add tracking fields and enhance carpooling model; update views for better layout

--- models/carpooling.py
+++ models/carpooling.py
@@ -1,18 +1,27 @@
 #-*- coding: utf-8 -*-
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+class TestAbstract(models.AbstractModel):
+    _name = 'test.abstract' 
+    _description = """
+        this is a model 
+    """
 
+    @api.onchange('name')
+    def change_my_name(self):
+        self.name = self.name + " suffixe"
 class Carpooling(models.Model):
     _name = "carpooling.carpooling"
     _description = """
         this is a model for carpooling
     """
-    name = fields.Char(string='Name')
-    taken_seats = fields.Integer(string="Taken Seats")
-    departure_time = fields.Float(string='Departure time')
-    departure_date = fields.Date(string='Departure date')
-    departure_city = fields.Char(string="departure city")
-    destination_city = fields.Char(string="Destination city")
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    name = fields.Char(string='Name', tracking=True)
+    taken_seats = fields.Integer(string="Taken Seats", tracking=True)
+    departure_time = fields.Float(string='Departure time', tracking=True)
+    departure_date = fields.Date(string='Departure date', tracking=True)
+    departure_city = fields.Char(string="departure city", tracking=True)
+    destination_city = fields.Char(string="Destination city", tracking=True)
     note = fields.Text('Note')
     resume = fields.Html('resume')
     image = fields.Binary(string="image")
@@ -20,10 +29,10 @@ class Carpooling(models.Model):
     states = [('new', 'New'), ('available', 'Has seats available'), ('full', 'Full')]
     state = fields.Selection(selection=states, default='new')
     company_currency = fields.Many2one('res.currency', string='currency', compute="_compute_company_currency")
-    amount_per_km = fields.Monetary(string="Amount per km", currency_field="company_currency")
+    amount_per_km = fields.Monetary(string="Amount per km", currency_field="company_currency", tracking=True)
     car_id = fields.Many2one('carpooling.car', string="Car", required=True)
-    km = fields.Float(string="Distance", required=True)
-    cost = fields.Monetary(string="cost", currency_field="company_currency", compute="_calcul_cost", store=True)
+    km = fields.Float(string="Distance", required=True, tracking=True)
+    cost = fields.Monetary(string="cost", currency_field="company_currency", compute="_calcul_cost", store=True, tracking=True)
     @api.constrains('km')
     def _check_km(self):
         for rec in self:
